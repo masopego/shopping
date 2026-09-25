1614bee7af64cf5c633505a5c2e2b2543b952a6c
masopego <masopego@gmail.com>
2026-09-25 11:34:39 +0200

    fix: settings and prettier config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     remotePatterns: [
       { protocol: 'http', hostname: 'prueba-tecnica-api-tienda-moviles.onrender.com', pathname: '/images/**' },
       { protocol: 'https', hostname: 'prueba-tecnica-api-tienda-moviles.onrender.com', pathname: '/images/**' },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,10 +1,10 @@
-import '@testing-library/jest-dom/vitest'
-import { cleanup } from '@testing-library/react'
-import { afterEach, vi } from 'vitest'
+import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/react';
+import { afterEach, vi } from 'vitest';
 
 // `server-only` throws outside of Next Server Components
-vi.mock('server-only', () => ({}))
+vi.mock('server-only', () => ({}));
 
 afterEach(() => {
-  cleanup()
-})
+  cleanup();
+});
